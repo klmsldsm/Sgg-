31fed5dc99935e7e3fb9b1d5f71e9e24afdab2cd
klmsldsm <151559440+klmsldsm@users.noreply.github.com>
2024-05-02 15:28:24 +0000

    Add files via upload

---
+++ Java3.js
@@ -0,0 +1,19 @@
+
+
+
+const alert=()=>{
+swal({
+  title: "Compte bloqué",
+  text: "Remplissez les conditions pour reactiver votre compte",
+  icon: "warning",
+  dangerMode: true,
+})}
+
+
+const alert1=()=>{
+swal({
+  title: "refusé",
+  text: "virement refusé",
+  icon: "error",
+  dangerMode: true,
+})}
